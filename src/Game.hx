70f07199467b749c796f54297b6e6cdf4c9486c1
Cameron Gaskin <cg695@nau.edu>
2015-02-26 18:09:18 -0700

    bearfact error fix

--- src/Game.hx
+++ src/Game.hx
@@ -57,11 +57,8 @@ class Game extends Sprite {
 									  "berrybush", "berrybush", 200, 300, player, "test", "test", 1, 3, ""));
 
 		encounters.push(new Encounter("bear", "Its a bear!", ["Fight!", "Run", "Play dead"], 2, 
-									  "Good Job!", 
-
-									  "Oh no! The bear ate you. \n\n Grizzly Bears can run up to 30 mph. (Bring a slow friend).",
-									  "", "bear", 500, 500, player, "test", "test", 100, 101));
-
+									  "Good Job!", "Oh no! The bear ate you. \n\n Grizzly Bears can run up to 30 mph. (Bring a slow friend).",
+									  "", "bear", 500, 500, player, "test", "test", 100, 101, ""));
 		
 		encounters.push(new Encounter("log", "You found a log!", ["Build a Fire", "Eat the Log"], 1, 
 									  "You shouldn't eat the log. Wood is hard to digest",
