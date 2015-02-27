f874d075df29814c115c8c3f76204dd354008ce0
BlackAR <anthony.r.black13@gmail.com>
2015-02-26 18:47:08 -0700

    Updated river text

--- src/Game.hx
+++ src/Game.hx
@@ -42,14 +42,18 @@ class Game extends Sprite {
 
 		//Add encounters
 		encounters = new Array<Encounter>();
+		encounters.push(new Encounter("river", "You found a river!", ["Drink water", "Walk away"], 1, 
+									  "Good job! Stagnant water should be treated before drinking.",
+									  "Oh no! Stagnant water is the perfect breeding ground for bacteria and parasites, which can lead to serious sickness!",
+									  "","", 0, 0, player, "Fill canteen", "canteen", 0, 3, ""));
 		encounters.push(new Encounter("berrybush", "Its a berry bush! You are unable to idenitify the type of berries it has. What do you do?", ["Eat the berries", "Leave them alone"], 1, 
 									  "Good Job! You should never eat something you are unable identify.", 
 									  "Oh no! The berries are poisonous! You should never eat anything you can not identify.", 
-									  "berrybush", "berrybush", 200, 300, player, "test", "test", 1, 3, ""));
+									  "", "", 200, 300, player, "test", "test", 1, 3, ""));
 
 		encounters.push(new Encounter("bear", "Its a bear!", ["Fight!", "Run", "Play dead"], 2, 
 									  "Good Job!", "Oh no! The bear ate you. \n\n Grizzly Bears can run up to 30 mph. (Bring a slow friend).",
-									  "", "bear", 500, 500, player, "test", "test", 100, 101, ""));
+									  "", "", 500, 500, player, "test", "test", 100, 101, ""));
 		
 		encounters.push(new Encounter("log", "You found a log!", ["Build a Fire", "Eat the Log"], 0, 
 									  "Good Job! Hardwoods, like Aspen, burn longer and hotter than softer woods.",
@@ -59,11 +63,7 @@ class Game extends Sprite {
 		 encounters.push(new Encounter("canteen", "You found a canteen!", ["Eat the Canteen", "Pickup the Canteen"], 1, 
 									  "Good job! Now you can fill the canteen with fresh water. Make sure your water source is not stagnant",
 									  "Food might be in limited supply, but you shouldn't eat the canteen",
-									  "","", 200, 75, player, "test", "test", 0, 3, ""));
- 		 encounters.push(new Encounter("river", "You found a canteen!", ["Eat the Canteen", "Pickup the Canteen"], 1, 
-									  "Good job! Now you can fill the canteen with fresh water. Make sure your water source is not stagnant",
-									  "Food might be in limited supply, but you shouldn't eat the canteen",
-									  "","", 0, 0, player, "test", "test", 0, 3, ""));
+									  "","", 200, 75, player, "test", "test", 0, 3, "canteen"));
 
 		//Add encounters to stage
 		for (encounter in encounters) {
